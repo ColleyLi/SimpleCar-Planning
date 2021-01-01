3dc75422a9ecf65e22b9b2b3eb812a738a61a819
CosmicRadiance <74691183+CosmicRadiance@users.noreply.github.com>
2021-01-01 23:03:01 +0100

    convert original refrence .txt file to make it applicable in ACADO

---
+++ data_convert.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 31 21:40:33 2020
+
+@author: Yulei Qiu
+"""
+
+import numpy as np
+
+a = np.loadtxt('simple_car_path_app.txt') # load the reference as a 2D ndarray
+
+b = np.delete(a, np.s_[3:5], 1) # delete control elements
+# b = a
+
+# c = b[:,[5,0,1,2,3,4]]
+c = b[:,[3,0,1,2]] # rearrange the columns
+
+
+c[:,0] = np.cumsum(c[:,0], axis=0) # calculate the time stamp
+
+d = np.savetxt('path.txt', c,  '%.5f') # save it to a .txt file
