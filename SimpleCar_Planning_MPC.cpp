10c9d6cba79be020faa91e3f8be01524722df870
CosmicRadiance <74691183+CosmicRadiance@users.noreply.github.com>
2021-01-08 20:30:24 +0100

    Update SimpleCar_Planning_MPC.cpp

--- SimpleCar_Planning_MPC.cpp
+++ SimpleCar_Planning_MPC.cpp
@@ -23,7 +23,7 @@ int main( )
 
 	f << dot(x) == U0*cos(theta);
 	f << dot(y) == U0*sin(theta);
-	f << dot(theta) == U0*tan(U1) / 0.3;  // 0.3 is the wheelbase length
+	f << dot(theta) == U0*tan(U1) / 1.0;  // the wheelbase length
 
 
 
@@ -32,91 +32,81 @@ int main( )
 	OutputFcn identity;
 	DynamicSystem dynamicSystem( f,identity );
 
-	Process process( dynamicSystem,INT_RK78 );
+	Process process( dynamicSystem,INT_RK45 );
 
 
     // DEFINE LEAST SQUARE FUNCTION:
     // -----------------------------
     Function h1;
 
-    h1 << x;
-    h1 << y;
-    h1 << theta;
+    h1 << x << y << theta << U0 << U1;
 
-
-    DMatrix Q(3,3);
+    DMatrix Q(5,5);
     Q.setIdentity();
-	Q(0,0) = 0.1;
-	Q(1,1) = 0.1;
-	Q(2,2) = 0.1;
+	Q(0,0) = 1.0;
+	Q(1,1) = 1.0;
+	Q(2,2) = 1.0;
+        Q(3,3) = 1.0;
+        Q(4,4) = 1.0;
 
-
-    DVector r(3);
-    r.setAll( 0.0 );
-        //r(0) = 26; 
-        //r(1) = -20;
-        //r(2) = 3.14145;
+    DVector r(5);
+        r(0) = 9;
+        r(1) = 9;
+        r(2) = 0.75;
+        r(3) = 0.2;
+        r(4) = 0.2;
 
 
     Function h2;
-
-    h2 << x;
-    h2 << y;
-    h2 << theta;
-
+    h2 << x << y << theta;
 
     DMatrix Q_end(3,3);
     Q_end.setIdentity();
-	Q_end(0,0) = 1.0;
-	Q_end(1,1) = 1.0;
-	Q_end(2,2) = 1.0;
+	Q_end(0,0) = 0.1;
+	Q_end(1,1) = 0.1;
+	Q_end(2,2) = 0.1;
 
-
-    DVector r_end(3);
-        //r.setAll( 0.0 );
-        //r_end(0) = 26.01760; 
-        //r_end(1) = -0.81004;
-        //r_end(2) = 3.14145;
+    //DVector r_end(3);
+        r_end(0) = 18;
+        r_end(1) = 18;
+        r_end(2) = 1.5;
 
 
     // DEFINE AN OPTIMAL CONTROL PROBLEM:
     // ----------------------------------
     const double t_start = 0.0;
-    const double t_end   = 5.0;
+    const int N = 10;
+    const double dt = 0.1;
+    const double t_end = t_start + N*dt;
 
-    OCP ocp( t_start, t_end, 15 );
+    OCP ocp( t_start, t_end, N );
 
-    ocp.minimizeLSQ( Q, h1, r );
-    //ocp.minimizeLSQEndTerm( Q_end, h2, r_end );
+    ocp.minimizeLSQ( Q, h1, r);
+    //ocp.minimizeLSQEndTerm( Q_end, h2, r_end);
 
 	ocp.subjectTo( f );
-
-	ocp.subjectTo( -1.5 <= U0 <= 1.5 );
-        ocp.subjectTo( -0.6 <= U1 <= 0.6 );
-        ocp.subjectTo( -3.1416 <= theta <= 3.1416 );
+	ocp.subjectTo( -10.0 <= U0 <= 10.0 );
+        ocp.subjectTo( -M_PI/4 <= U1 <= M_PI/4 );
+        ocp.subjectTo( -M_PI <= theta <= M_PI );
 
 
     // SETTING UP THE MPC CONTROLLER:
     // ------------------------------
-	RealTimeAlgorithm alg( ocp,0.1 );
-	alg.set( KKT_TOLERANCE, 1e-04 );
-        alg.set( INTEGRATOR_TYPE, INT_RK78);
-        //alg.set( PLOT_RESOLUTION, MEDIUM ) ;
-	
         VariablesGrid myReference; myReference.read( "path.txt" );// read the reference
         StaticReferenceTrajectory reference( myReference );
 
-	Controller controller( alg,reference );
 
+	RealTimeAlgorithm alg( ocp, dt );
+	Controller controller( alg, reference );
 
 
     // SETTING UP THE SIMULATION ENVIRONMENT:
     // --------------------------------------
-	SimulationEnvironment sim( 0.0,160.0,process,controller );
+	SimulationEnvironment sim( 0.0, 120.0, process, controller );
 
 	DVector x0(3);
-	x0(0) = 0.0;
-	x0(1) = 0.0;
+	x0(0) = 2.0;
+	x0(1) = 2.0;
 	x0(2) = 0.0;
 
 
@@ -126,27 +116,33 @@ int main( )
 		exit( EXIT_FAILURE );
 
 
-    // ...AND PLOT THE RESULTS
+    // ...AND PRINT THE RESULTS:
     // ----------------------------------------------------------
-	VariablesGrid diffStates;
-	sim.getProcessDifferentialStates( diffStates );
+	VariablesGrid outputStates;
+	VariablesGrid outputControls;
 
-	VariablesGrid feedbackControl;
-	sim.getFeedbackControl( feedbackControl );
+	sim.getProcessDifferentialStates( outputStates );
+        sim.getFeedbackControl( outputControls );
 
-	//GnuplotWindow window;
-	//window.addSubplot( diffStates(0), "x [m]" );
-	//window.addSubplot( diffStates(1), "y [m]" );
-	//window.addSubplot( diffStates(2), "theta [rad]" );
-	//window.addSubplot( feedbackControl(0),      "velocity [m/s]" );
-	//window.addSubplot( feedbackControl(1),      "steering angle [rad]" );
-	//window.plot();
-        //diffStates.printToFile(  );
-        diffStates.print( "pathMPC.txt" );
+        ofstream output_states = std::ofstream("output_states.txt");
+        ofstream output_controls = std::ofstream("output_controls.txt");
+
+        //outputStates.print( "output_states.txt" );
+
+        for (auto i=0; i<outputStates.getDim()/outputStates.getNumRows()/outputStates.getNumCols(); i++)
+        {
+          output_states << outputStates(i,0) <<" "<<
+                           outputStates(i,1) <<" "<<
+                           outputStates(i,2) <<"\n";
+        }
+
+        for (auto i=0; i<outputControls.getDim()/outputControls.getNumRows()/outputControls.getNumCols(); i++)
+        {
+          output_controls << outputControls(i,0) <<" "<<
+                             outputControls(i,1) <<"\n";
+        }
 
 
     return 0;
 }
-
-
 
