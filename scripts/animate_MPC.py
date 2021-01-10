38c15b437b2d039de468327728e54f7cd8295acb
Yulei Qiu <vsper2016@gmail.com>
2021-01-10 17:04:11 +0100

    Animation showing the tracking process

--- scripts/animate_MPC.py
+++ scripts/animate_MPC.py
@@ -2,37 +2,27 @@
 """
 Visualize the path using Matplotlib. Show the animation of tracking the path.
 
-@author: Yulei Qiu
+@author: Jianfeng Cui, Yulei Qiu
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 import plot_square as ps
 
-# Load data from different approaches and scenarios
+# load data from different approaches and scenarios
 data1 = np.loadtxt('../data/simple_car_path_geometric.txt')
 data2 = np.loadtxt('../data/simple_car_path_geometric_app.txt')
 data3 = np.loadtxt('../data/output_states.txt')
 data4 = np.loadtxt('../data/output_states_app.txt')
 
-# Plot path(note: grouped as 1 with 3, 2 with 4)
-# =============================================================================
-# fig, axes = plt.subplots(1, 2, figsize=(10, 4))
-# line1, = axes[0].plot(data1[:,0], data1[:,1], 'orange', linewidth=1.5, alpha=0.8, label='ref')
-# line2, = axes[1].plot(data2[:,0], data2[:,1], 'orange', linewidth=1.5, alpha=0.8, label='ref')
-# line3, = axes[0].plot(data3[:,0], data3[:,1], 'blue', linewidth=1.5, alpha=0.8, label='mpc')
-# line4, = axes[1].plot(data4[:,0], data4[:,1], 'blue', linewidth=1.5, alpha=0.8, label='mpc')
-# axes[0].legend(handles=[line1, line3])
-# axes[1].legend(handles=[line2, line4])
-# =============================================================================
+# plot the reference track
 fig = plt.figure(figsize=[10,10])
 ax = fig.gca()
 ax.plot(data1[:,0], data1[:,1], 'orange', linewidth=1.5, alpha=0.8, label='ref')
 ax.set_aspect("equal")
 
-ox, oy = [], []
 # obstacles
+ox, oy = [], []
 # transform the index from 2D vector to the grid: 20 - i
 for i in np.arange(90, 100+1, 1):
 	for j in np.arange(90, 100+1, 1):
@@ -61,9 +51,7 @@ ax.plot(ox, oy, 'sk')
 ax.scatter(20.0, 20.0, c='cyan')
 ax.scatter(180.0, 180.0, c='magenta')
 
-# arrow = patches.Arrow( data3[0,0], data3[0,1], 10*(data3[0+1,0]-data3[0,0]), 10*(data3[0+1,1]-data3[0,1]) )
-# a = ax.add_patch(arrow)
-
+# animate the tracking process
 plt.pause(5.0)
 for i in range(0, data3.shape[0], 80):
     line_square = ps.plot_vehicle_state(ax, data3[i,:])
